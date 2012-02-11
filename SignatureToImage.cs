736dceffeb1197733d4795f0189de2d8410cfd35
Justin Stolle <justin@justinstolle.com>
2012-02-10 16:39:47 -0800

    removed dependency on Newtonsoft.Json

--- SignatureToImage.cs
+++ SignatureToImage.cs
@@ -1,16 +1,16 @@
 /*
  * Author:      Curtis Herbert (me@forgottenexpanse.com)
  * License:     BSD License 
- * Version: 	1.0 (2011-03-31)
+ * Version: 	1.1 (2011-10-03)
  */
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Drawing;
-using Newtonsoft.Json;
 using System.Drawing.Text;
 using System.Drawing.Drawing2D;
+using System.Web.Script.Serialization;
 
 namespace ConsumedByCode.SignatureToImage
 {
@@ -56,7 +56,8 @@ namespace ConsumedByCode.SignatureToImage
                 signatureGraphic.Clear(Background);
                 signatureGraphic.SmoothingMode = SmoothingMode.AntiAlias;
                 Pen pen = new Pen(PenColor, PenWidth);
-                List<SignatureLine> lines = (List<SignatureLine>)JsonConvert.DeserializeObject(json ?? string.Empty, typeof(List<SignatureLine>));
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                List<SignatureLine> lines = serializer.Deserialize<List<SignatureLine>>(json ?? string.Empty);
                 foreach (SignatureLine line in lines)
                 {
                     signatureGraphic.DrawLine(pen, line.lx, line.ly, line.mx, line.my);
